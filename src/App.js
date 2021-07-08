92d34f370e9af22d443be74f2b979b9b1b05cc08
Mark <mark.nelubin@gmail.com>
2021-07-08 13:00:12 +0300

    init new version of project

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import { useState } from "react";
-import Form from "./Form";
-import Users from "./Users.js";
-import Popup from "./Popup";
-
 
 function App() {
-  const [users, setUsers] = useState([
-    { name: "Mark", age: 30, id: 1 },
-    { name: "Sveta", age: 28, id: 2 },
-    { name: "Alex", age: 31, id: 3 }
-  ]);
-
-  const [showPopup, setshowPopup] = useState([false, false]);
-
-  const handleUsers = (user) => {
-    setUsers(prevUsers => {
-      const updatedUsers = [...prevUsers];
-      updatedUsers.unshift(user);
-      return updatedUsers;
-    });
-  }
-
-  const handlePopup = (negativeNumber, emptyInputs) => {
-    setshowPopup([negativeNumber, emptyInputs]);
-  }
 
   return (
     <div className="App">
-      <Form handleUsers={handleUsers} handlePopup={handlePopup} />
-      <Users users={users} />
-      <Popup showPopup={showPopup} handlePopup={handlePopup} />
+      <h1>Hello world!</h1>
     </div>
   );
 }
